8aa354e0168c7ca7380ae45fe161c3758c04169c
Josue <josue.chaqui@gmail.com>
2013-02-27 18:38:38 -0600

    agregando una nueva clase

---
+++ src/interfazgraficamonticulos/UltimaLinea.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfazgraficamonticulos;
+
+/**
+ *
+ * @author chaqui
+ */
+public class UltimaLinea {
+    int x;
+    int y;
+    int size;
+    
+}
