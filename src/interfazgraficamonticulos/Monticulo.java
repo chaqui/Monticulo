8d5de2a23a5c7fe6b7aa76f785994d83bf3719b7
Josue <josue.chaqui@gmail.com>
2013-03-03 17:50:14 -0600

    arreglado error linea 51 gracias @tuxtor

--- src/interfazgraficamonticulos/Monticulo.java
+++ src/interfazgraficamonticulos/Monticulo.java
@@ -47,11 +47,12 @@ public class Monticulo {
             if (cPadre>=0) {
                 if (this.usuario[cPadre].getClave()>this.usuario[size].getClave()) {
                     this.upHeap(cPadre, size);
+                    size++;
                 }
             }
         }
         }
-        size++;
+        
         
         
     }
